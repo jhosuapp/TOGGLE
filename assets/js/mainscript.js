3cbcee9ca33ff8a047e0fd08e6edf594610acf2b
Jhosua <jhosuapvll@gmail.com>
2022-02-11 14:51:36 -0500

    Se termina el toggle

--- assets/js/mainscript.js
+++ assets/js/mainscript.js
@@ -5,31 +5,41 @@ getOnePanel.style.maxHeight = getOnePanel.scrollHeight + "px";
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    var panel = this.nextElementSibling;
-    var parentToggle = this.parentNode;
 
-    // this.classList.toggle("active"); 
-    // parentToggle.classList.toggle('active');
+    var panel = this.nextElementSibling;
+    var parentToggle = this.parentNode; 
 
     this.classList.toggle('panel_active');
     var getAllPanelActive = document.querySelectorAll('.panel_active');
 
     if(getAllPanelActive.length > 0){
+
       var getAllAccordion = document.querySelectorAll('.accordion__panel');
       getAllAccordion.forEach((dataTwo)=>{
         dataTwo.style.maxHeight = null;
       });
+
       getAllPanelActive.forEach( (data)=>{
         data.classList.remove('panel_active');
       });
+
+      var getAllContainers = document.querySelectorAll('.accordion__container');
+      getAllContainers.forEach((data)=>{
+        data.classList.remove('active');
+      });
+
+      var getAccordion = document.querySelectorAll('.accordion');
+      getAccordion.forEach((data)=>{
+        data.classList.remove('active');
+      });
     } 
 
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
+    if (1 < 2) {
       panel.style.maxHeight = panel.scrollHeight + "px";
+      this.classList.add("active"); 
+      parentToggle.classList.add('active');
     } 
-
+    
   });
 }
 
